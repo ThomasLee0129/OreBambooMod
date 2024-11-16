68679366ec88aa75c6d8acdf147428023f40e50a
thomas <thomaslee0129@outlook.com>
2024-11-16 14:22:59 +0800

    new OreBamboo

--- src/main/java/lee/orebamboo/orebamboomod/datagen/Datagen.java
+++ src/main/java/lee/orebamboo/orebamboomod/datagen/Datagen.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 public class Datagen implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator generator){
+
         FabricDataGenerator.Pack pack = generator.createPack();
 
         pack.addProvider(DataLootTableGenerator::new);
