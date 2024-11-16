ec98e546fd7f1a40f558c85fe663df2931ba0ec0
thomas <thomaslee0129@outlook.com>
2024-11-16 15:50:32 +0800

    new OreBamboo

--- src/main/java/lee/orebamboo/orebamboomod/Orebamboomod.java
+++ src/main/java/lee/orebamboo/orebamboomod/Orebamboomod.java
@@ -4,6 +4,7 @@ import lee.orebamboo.orebamboomod.orebambooitemGroup.ModItemGroup;
 import lee.orebamboo.orebamboomod.orebambooBlock.ModBlockRegister;
 import lee.orebamboo.orebamboomod.orebambooItem.ModItemRegister;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 
 public class Orebamboomod implements ModInitializer {
 
@@ -14,6 +15,12 @@ public class Orebamboomod implements ModInitializer {
         ModItemRegister.initialize();
         ModBlockRegister.initialize();
         ModItemGroup.initialize();
+
+        FuelRegistry.INSTANCE.add(ModItemRegister.COAL_BAMBOO_ITEM,2000);
+        FuelRegistry.INSTANCE.add(ModItemRegister.GOLD_BAMBOO_ITEM,3200);
+        FuelRegistry.INSTANCE.add(ModItemRegister.DIAMOND_BAMBOO_ITEM,4800);
+        FuelRegistry.INSTANCE.add(ModItemRegister.EMERALD_BAMBOO_ITEM,6400);
+        FuelRegistry.INSTANCE.add(ModItemRegister.NETHERITE_BAMBOO_ITEM,7200);
 
     }
 }
